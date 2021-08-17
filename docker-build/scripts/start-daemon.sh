d9e13ab119ca16d1bc804271ece3bfc3c29b29a5
shoothzj <shoothzj@gmail.com>
2021-08-17 21:03:34 +0800

    cluster init script exit

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-nohup $PULSAR_HOME/mate/pulsar_mate >$PULSAR_HOME/pulsar_mate.stdout.log 2>$PULSAR_HOME/pulsar_mate.stderr.log
+
+if [ $CLUSTER_INIT == "true" ]; then
+    $PULSAR_HOME/mate/pulsar_mate 2>&1
+else
+    nohup $PULSAR_HOME/mate/pulsar_mate >$PULSAR_HOME/pulsar_mate.stdout.log 2>$PULSAR_HOME/pulsar_mate.stderr.log
+fi
