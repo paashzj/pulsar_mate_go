d19b434669a8777e8c30a195f5504c521cefe27b
shoothzj <shoothzj@gmail.com>
2021-08-28 20:07:17 +0800

    Fix wrong log level

--- pkg/pulsar/start.go
+++ pkg/pulsar/start.go
@@ -11,7 +11,7 @@ import (
 
 func StartInit() {
 	stdout, stderr, err := gutil.CallScript(path.PulsarInitScript)
-	util.Logger().Error("shell result ", zap.String("stdout", stdout), zap.String("stderr", stderr))
+	util.Logger().Info("shell result ", zap.String("stdout", stdout), zap.String("stderr", stderr))
 	if err != nil {
 		util.Logger().Error("pulsar cluster init failed ", zap.Error(err))
 	} else {
@@ -22,22 +22,22 @@ func StartInit() {
 func StartOther() {
 	if config.Function {
 		stdout, stderr, err := gutil.CallScript(path.PulsarStartFunctionScript)
-		util.Logger().Error("shell result ", zap.String("stdout", stdout), zap.String("stderr", stderr))
 		if err != nil {
 			util.Logger().Error("start pulsar function server failed ", zap.Error(err))
 			os.Exit(1)
 		}
+		util.Logger().Info("shell result ", zap.String("stdout", stdout), zap.String("stderr", stderr))
 	}
 	if config.ClusterEnable {
 		stdout, stderr, err := gutil.CallScript(path.PulsarStartScript)
-		util.Logger().Error("shell result ", zap.String("stdout", stdout), zap.String("stderr", stderr))
+		util.Logger().Info("shell result ", zap.String("stdout", stdout), zap.String("stderr", stderr))
 		if err != nil {
 			util.Logger().Error("start pulsar server failed ", zap.Error(err))
 			os.Exit(1)
 		}
 	} else {
 		stdout, stderr, err := gutil.CallScript(path.PulsarStartStandaloneScript)
-		util.Logger().Error("shell result ", zap.String("stdout", stdout), zap.String("stderr", stderr))
+		util.Logger().Info("shell result ", zap.String("stdout", stdout), zap.String("stderr", stderr))
 		if err != nil {
 			util.Logger().Error("start pulsar server failed ", zap.Error(err))
 			os.Exit(1)
