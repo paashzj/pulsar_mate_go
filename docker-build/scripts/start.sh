294d1916170e1c9685b82f9b3ad9d49cc0128aed
lovehzj <1922919664@qq.com>
2022-01-09 14:41:24 +0800

    fix mate log path

--- docker-build/scripts/start.sh
+++ docker-build/scripts/start.sh
@@ -4,6 +4,7 @@ mkdir $PULSAR_HOME/logs
 if [ $CLUSTER_INIT == "true" ]; then
     $PULSAR_HOME/mate/pulsar_mate 2>&1
 else
-    nohup $PULSAR_HOME/mate/pulsar_mate >$PULSAR_HOME/logs/pulsar_mate.stdout.log 2>$PULSAR_HOME/logs/pulsar_mate.stderr.log
+    nohup $PULSAR_HOME/mate/pulsar_mate >>$PULSAR_HOME/logs/pulsar_mate.stdout.log 2>>$PULSAR_HOME/logs/pulsar_mate.stderr.log
     tail -f /dev/null
 fi
+
