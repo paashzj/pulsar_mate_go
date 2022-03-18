355c5bd241790549c6fe3993854bffe6968a2c8a
goflutterjava <goflutterjava@gmail.com>
2022-03-18 17:44:41 +0800

    fix daemon script hang

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p $PULSAR_HOME/logs
-nohup $PULSAR_HOME/mate/pulsar_mate >>$PULSAR_HOME/logs/pulsar_mate.stdout.log 2>>$PULSAR_HOME/logs/pulsar_mate.stderr.log
+nohup $PULSAR_HOME/mate/pulsar_mate >>$PULSAR_HOME/logs/pulsar_mate.stdout.log 2>>$PULSAR_HOME/logs/pulsar_mate.stderr.log &
 
